214df0e69b3e420c6440a71f10fa0bb5da5106fb
deathgodsato <kekigenkai@live.ca>
2014-06-03 15:38:17 -0400

    Revert "test thingy"
    This reverts commit f089a8beb19e4fe89bf9d572de5aee6277b4f9c8.

--- DeathAir/src/com/example/deathair/framework/Audio.java
+++
@@ -1,7 +0,0 @@
-package com.example.deathair.framework;
-
-public interface Audio {
-    public Music newMusic(String filename);
-
-    public Sound newSound(String filename);
-}
--- DeathAir/src/com/example/deathair/framework/Sound.java
+++
@@ -1,7 +0,0 @@
-package com.example.deathair.framework;
-
-public interface Sound {
-    public void play(float volume);
-
-    public void dispose();
-}
