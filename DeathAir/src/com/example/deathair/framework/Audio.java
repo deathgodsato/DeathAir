f089a8beb19e4fe89bf9d572de5aee6277b4f9c8
deathgodsato <kekigenkai@live.ca>
2014-06-03 15:33:30 -0400

    test thingy

---
+++ DeathAir/src/com/example/deathair/framework/Audio.java
@@ -0,0 +1,7 @@
+package com.example.deathair.framework;
+
+public interface Audio {
+    public Music newMusic(String filename);
+
+    public Sound newSound(String filename);
+}
---
+++ DeathAir/src/com/example/deathair/framework/Sound.java
@@ -0,0 +1,7 @@
+package com.example.deathair.framework;
+
+public interface Sound {
+    public void play(float volume);
+
+    public void dispose();
+}
